0539f50cc3f3cce47cabff71ab4362452e1b92db
finbar-rollbar <73967662+finbar-rollbar@users.noreply.github.com>
2022-07-24 20:25:16 -0400

    Update RollbarConfig.java
    Read environment data from application.properties src/main/resources

--- src/main/java/com/example/demo/RollbarConfig.java
+++ src/main/java/com/example/demo/RollbarConfig.java
@@ -1,26 +1,58 @@
 package com.example.demo;
 
+import com.rollbar.notifier.Rollbar;
+
 import com.rollbar.notifier.config.Config;
 import com.rollbar.notifier.config.ConfigBuilder;
 import com.rollbar.notifier.config.ConfigProvider;
 import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+@Configuration()
+@EnableWebMvc
+@ComponentScan({
+    "com.example.demo",
+    "com.rollbar.spring"
+})
 public class RollbarConfig implements ConfigProvider {
 
+  @Autowired
+  private Environment env;
   
+  @Bean
+  public Rollbar rollbar() {
+
+    ConfigBuilder cb = ConfigBuilder.withAccessToken("");
+    Rollbar rb = new Rollbar(provide(cb));
+    rb.info("RollbarConfig completing");
+    return rb;
+  }
+
+
   @Override
   public Config provide(ConfigBuilder configBuilder) {
 
-    // Set environment and (code) version as part of yuor build process
+    String accessToken = env.getProperty("x.accessToken");
+    String environment = env.getProperty("x.environment");
+    String codeVersion = env.getProperty("x.codeVersion");
+
+    System.out.println(accessToken);
 
     // Reference ConfigBuilder.java for all the properties you can set for Rollbar
-    return RollbarSpringConfigBuilder.withAccessToken(RollbarSetting.accessToken)
-        .environment(RollbarSetting.environment)
-        .codeVersion(RollbarSetting.codeVersion)
+    return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+        .environment(environment)
+        .codeVersion(codeVersion)
         .server(new ServerProvider())
         .person(new PersonProvider())
         .transformer(new PayloadTransformer())
         .build();
   }
 
-  }
+}
